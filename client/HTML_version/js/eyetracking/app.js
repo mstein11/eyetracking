0adc5f8a64bdd5cf5038aacd67ad6b62fc79e1bf
Marius Stein <mariusstein7@gmail.com>
2015-05-15 23:55:00 +0200

    cpu load test

--- client/HTML_version/js/eyetracking/app.js
+++ client/HTML_version/js/eyetracking/app.js
@@ -9,6 +9,22 @@ $(document).ready(function() {
             method: 'GET',
             success: function(data) {
                 console.log("success!: " + data)
+
+                var html = '<div class="easy-pie-chart txt-color-orangeDark" data-percent="'+data+'" data-pie-size="50">' +
+                    '<span class="percent percent-sign">' + data + '</span>' +
+                    '</div>' +
+                    '<span class="easy-pie-title"> Server Load <i class="fa fa-caret-up icon-color-bad"></i> </span> ' +
+                    '<ul class="smaller-stat hidden-sm pull-right"> ' +
+                    '<li> ' +
+                    '<span class="label bg-color-greenLight"><i class="fa fa-caret-up"></i> 97%</span> ' +
+                    '</li> ' +
+                    '<li> ' +
+                    '<span class="label bg-color-blueLight"><i class="fa fa-caret-down"></i> 44%</span> ' +
+                    '</li> ' +
+                    '</ul>';
+
+                $('#cpu-load').html(html);
+
             },
             error: function(data) {
                 console.log(data);
