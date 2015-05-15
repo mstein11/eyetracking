9a03bedc3913bc79690e9af9ba8c90228b806598
Marius Stein <mariusstein7@gmail.com>
2015-05-16 00:15:26 +0200

    testing cpu usage

--- api/server.php
+++ api/server.php
@@ -1,5 +1,4 @@
 <?php
-echo $_GET;
 if (isset($_GET["getCpuLoad"])) {
     $load = sys_getloadavg();
     echo $load[0];
--- client/HTML_version/js/eyetracking/app.js
+++ client/HTML_version/js/eyetracking/app.js
@@ -2,7 +2,7 @@
  * Created by mariusstein on 15.05.15.
  */
 $(document).ready(function() {
-
+    var isCpuDrawnOnce = false;
     var getCpuLoadFunc = function() {
         $.ajax({
             url:'/api/server.php?getCpuLoad',
@@ -10,20 +10,28 @@ $(document).ready(function() {
             success: function(data) {
                 console.log("success!: " + data)
 
-                var html = '<div class="easy-pie-chart txt-color-orangeDark" data-percent="'+data+'" data-pie-size="50">' +
-                    '<span class="percent percent-sign">' + data + '</span>' +
-                    '</div>' +
-                    '<span class="easy-pie-title"> Server Load <i class="fa fa-caret-up icon-color-bad"></i> </span> ' +
-                    '<ul class="smaller-stat hidden-sm pull-right"> ' +
-                    '<li> ' +
-                    '<span class="label bg-color-greenLight"><i class="fa fa-caret-up"></i> 97%</span> ' +
-                    '</li> ' +
-                    '<li> ' +
-                    '<span class="label bg-color-blueLight"><i class="fa fa-caret-down"></i> 44%</span> ' +
-                    '</li> ' +
-                    '</ul>';
+                if (!isCpuDrawnOnce) {
 
-                $('#cpu-load').html(html);
+
+                    var html = '<div class="easy-pie-chart cpu-usage-pie-chart txt-color-orangeDark" data-percent="'+data+'" data-pie-size="50">' +
+                        '<span class="percent percent-sign">' + data + '</span>' +
+                        '</div>' +
+                        '<span class="easy-pie-title"> Server Load <i class="fa fa-caret-up icon-color-bad"></i> </span> ' +
+                        '<ul class="smaller-stat hidden-sm pull-right"> ' +
+                        '<li> ' +
+                        '<span class="label bg-color-greenLight"><i class="fa fa-caret-up"></i> 97%</span> ' +
+                        '</li> ' +
+                        '<li> ' +
+                        '<span class="label bg-color-blueLight"><i class="fa fa-caret-down"></i> 44%</span> ' +
+                        '</li> ' +
+                        '</ul>';
+
+                    $('#cpu-load').html(html);
+
+                    $('#cpu-usage-pie-chart').easyPieChart({
+                        //your configuration goes here
+                    });
+                }
 
             },
             error: function(data) {
