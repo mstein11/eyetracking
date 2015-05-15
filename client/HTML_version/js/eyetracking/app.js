57d01e3febe94b53fe57b58bffb8c1255cf5855b
Marius Stein <mariusstein7@gmail.com>
2015-05-15 22:28:58 +0200

    added js for cpu load

---
+++ client/HTML_version/js/eyetracking/app.js
@@ -0,0 +1,22 @@
+/**
+ * Created by mariusstein on 15.05.15.
+ */
+$(document).ready(function() {
+
+    var getCpuLoadFunc = function() {
+        $.ajax({
+            url:'/api/server.php?getCpuLoad',
+            method: 'GET',
+            success: function(data) {
+                alert("success: cpu usage is: " + data);
+            },
+            error: function(data) {
+                console.log(data);
+                //alert("error: cpu usage is: " + data);
+            }
+        })
+    }
+
+    window.setInterval(getCpuLoadFunc, 5000);
+
+});
